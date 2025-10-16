dfeafe028a4d678372fe922bd6be601d578bd7b8
mndekwe-dot <m.ndekwe@alustudent.com>
2025-10-16 16:14:01 +0200

    Fix print formatting for number output

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
+
 if number > 0:
-    print(f"{number} is positive")
+    print(f"{number:d} is positive")
 elif number == 0:
-    print(f"{number} is zero")
+    print(f"{number:d} is zero")
 else:
-    print(f"{number} is negative")
+    print(f"{number:d} is negative")
