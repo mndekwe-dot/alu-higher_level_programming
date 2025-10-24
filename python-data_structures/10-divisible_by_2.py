dbfdd179ff555da7e6143a1f89a41ba8145a4de7
mndekwe-dot <m.ndekwe@alustudent.com>
2025-10-24 12:06:32 +0200

    Update 10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,13 +1,5 @@
+#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    """
-    Finds all multiples of 2 in a list.
-    
-    Args:
-        my_list: The list of integers to check (default: empty list)
-    
-    Returns:
-        A new list with True or False for each element, indicating if it's divisible by 2
-    """
     result = []
     for num in my_list:
         result.append(num % 2 == 0)
