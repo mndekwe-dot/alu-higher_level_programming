1b4b00949bb83ddfcecc6ffcc26d61b2754c9e29
mndekwe-dot <m.ndekwe@alustudent.com>
2025-10-24 12:06:15 +0200

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,13 +1,5 @@
+#!/usr/bin/python3
 def max_integer(my_list=[]):
-    """
-    Finds the biggest integer in a list.
-    
-    Args:
-        my_list: The list of integers to search (default: empty list)
-    
-    Returns:
-        The maximum integer in the list, or None if the list is empty
-    """
     if not my_list:
         return None
     
