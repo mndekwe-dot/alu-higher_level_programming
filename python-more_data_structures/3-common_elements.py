f449629adb5a4102d787d3cd3fffc60673db9486
mndekwe-dot <m.ndekwe@alustudent.com>
2025-10-30 14:30:54 +0200

    Update 3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    """Return a set of common elements between two sets."""
-    return set_1 | set_2
+    """Return a set of common elements between two sets.
+
+    Args:
+        set_1: First set
+        set_2: Second set
+
+    Returns:
+        A new set containing elements present in both sets
+    """
+    return set_1 & set_2
