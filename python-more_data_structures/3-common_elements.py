63d9b414e0cba53f0f319ff4f378c0dfb424e06e
mndekwe-dot <m.ndekwe@alustudent.com>
2025-10-30 14:28:30 +0200

    Update 3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    """Return a set of common elements between two sets."""
-    return set_1 & set_2
-
+def raise_exception_msg(message=""):
+    raise NameError(message)
