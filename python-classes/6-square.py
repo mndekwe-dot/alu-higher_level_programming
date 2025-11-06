e31f82161fc0e296b8619b20e36d9bfe878ace3e
mndekwe-dot <m.ndekwe@alustudent.com>
2025-11-06 14:47:14 +0200

    Update 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
-
-
 class Square:
-    """Represents a square."""
-
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize the square.
-        Args:
-            size (int): size of the square (default 0)
-            position (tuple): position of the square (default (0, 0))
-        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Retrieve the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -30,12 +18,10 @@ class Square:
 
     @property
     def position(self):
-        """Retrieve the position."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Set the position with validation."""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
@@ -44,16 +30,13 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Return the area of the square."""
         return self.__size ** 2
 
     def my_print(self):
-        """Print the square with the character #."""
         if self.__size == 0:
             print("")
             return
 
-        # Print vertical spaces
         for _ in range(self.__position[1]):
             print("")
 
