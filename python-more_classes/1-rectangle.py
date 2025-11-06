2258be982912682feea80b38eeb9499f7bc4c755
mndekwe-dot <m.ndekwe@alustudent.com>
2025-11-06 15:33:51 +0200

    Create 1-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle."""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle
