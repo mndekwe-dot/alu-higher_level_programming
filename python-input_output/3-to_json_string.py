5d8562c3df2f73ad9eb09afc648b39a1def3c5c5
mndekwe-dot <m.ndekwe@alustudent.com>
2025-11-14 18:28:27 +0200

    Update 3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,10 +5,10 @@ import json
 
 def to_json_string(my_obj):
     """Returns the JSON representation of an object (string)
-    
+
     Args:
         my_obj: The object to convert to JSON string
-        
+
     Returns:
         str: JSON representation of the object
     """
