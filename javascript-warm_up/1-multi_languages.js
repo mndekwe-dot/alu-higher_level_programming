78c6092599743d99b23d6345cc511279decb29b4
mndekwe-dot <m.ndekwe@alustudent.com>
2026-01-29 13:00:22 +0200

    Update 1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const str1 = 'C is fun';
-const str2 = 'Python is cool';
-const str3 = 'JavaScript is amazing';
-console.log(str1);
-console.log(str2);
-console.log(str3);
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
